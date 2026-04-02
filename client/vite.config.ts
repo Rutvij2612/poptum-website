4c3c980338d96498bbc72f2d05401907fc705af7
Rutvij <rutvijborisagar@gmail.com>
2026-04-02 08:17:32 +0530

    fix: alias @ path for Vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1000 // increase limit (easy fix)
-  }
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
